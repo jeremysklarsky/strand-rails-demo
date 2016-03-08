c744ea95d4fa8d11bb45648bec33530bd3dba1f8
Jeremy Sklarsky <jsklarsky@mediamath.com>
2016-03-08 14:21:50 -0500

    add javascript file for index button demo

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,8 @@
+$( document ).ready(function() {
+  window.addEventListener("WebComponentsReady", function() { 
+      var coolBtn = document.querySelector("#coolBtn");
+      coolBtn.addEventListener("click", function(e) {
+          alert("coolBtn clicked!");
+      });
+  });   
+});
